8071e6f95df4668adc182e9d655d660fb9ab56b9
Bianca Aspin <bianca@mj94.net>
2023-02-04 16:31:15 -0800

    Refactor <select> element to be a controlled input.

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Filter({ onCategoryChange, search, onSearchChange }) {
+function Filter({ selectedCategory, onCategoryChange, search, onSearchChange }) {
   return (
     <div className="Filter">
       <input type="text" onChange={onSearchChange} name="search" placeholder="Search..." value={search}/>
-      <select name="filter" onChange={onCategoryChange}>
+      <select name="filter" value={selectedCategory} onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
